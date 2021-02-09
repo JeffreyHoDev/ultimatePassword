d201dc05ba5c820d19e964e728acc3b2e74721a6
Ho940106 <58864637+Ho940106@users.noreply.github.com>
2021-02-09 17:50:30 +0800

    add press key enter function to perform same as click submit, auto declare the loser when the only option left is one number

--- script.js
+++ script.js
@@ -10,7 +10,56 @@ $(document).ready(function(){
         $('.end-num').html(100)
         $('.real-num').html('?')
         answer = Math.round(Math.random()*100)
-        console.log(answer)
+
+        $('body').keypress(function (e) {
+            var key = e.which;
+            if(key == 13)  // the 'Enter' key code
+             {
+                var input = parseInt($('.input').val())
+                var startNum = parseInt($('.start-num').html()) 
+                var endNum = parseInt($('.end-num').html())
+                $('.input').val('')
+
+                // Validation of input
+                if(input >= endNum || input <= startNum){
+                    alert("Please input number within range")
+                }
+                else {
+                    if(answer === input){
+                        $('.real-num').html(answer)
+                        alert("You lose!")
+                        $('.startgame-btn').show()
+                        $('.submitans-btn').hide()
+                        $('.input').hide()
+                    }
+                    else if(answer < input){
+                        $('.end-num').html(input)
+                        startNum = parseInt($('.start-num').html()) 
+                        endNum = parseInt($('.end-num').html())
+                        if(endNum - startNum === 2){
+                            alert("Next Player Lose!")
+                            $('.real-num').html(answer)
+                            $('.startgame-btn').show()
+                            $('.submitans-btn').hide()
+                            $('.input').hide()
+                        }
+                    }
+                    else if(answer > input){
+                        $('.start-num').html(input)
+                        startNum = parseInt($('.start-num').html()) 
+                        endNum = parseInt($('.end-num').html())
+                        if(endNum - startNum === 2){
+                            alert("Next Player Lose!")
+                            $('.real-num').html(answer)
+                            $('.startgame-btn').show()
+                            $('.submitans-btn').hide()
+                            $('.input').hide()
+                        }
+                    }
+                }
+             }
+        });  
+
         $('.submitans-btn').click(function(){
             var input = parseInt($('.input').val())
             var startNum = parseInt($('.start-num').html()) 
@@ -23,16 +72,34 @@ $(document).ready(function(){
             else {
                 if(answer === input){
                     $('.real-num').html(answer)
-                    alert("You lose! Fker!")
+                    alert("You lose!")
                     $('.startgame-btn').show()
                     $('.submitans-btn').hide()
                     $('.input').hide()
                 }
                 else if(answer < input){
                     $('.end-num').html(input)
+                    startNum = parseInt($('.start-num').html()) 
+                    endNum = parseInt($('.end-num').html())
+                    if(endNum - startNum === 2){
+                        alert("Next Player Lose!")
+                        $('.real-num').html(answer)
+                        $('.startgame-btn').show()
+                        $('.submitans-btn').hide()
+                        $('.input').hide()
+                    }
                 }
                 else if(answer > input){
                     $('.start-num').html(input)
+                    startNum = parseInt($('.start-num').html()) 
+                    endNum = parseInt($('.end-num').html())
+                    if(endNum - startNum === 2){
+                        alert("Next Player Lose!")
+                        $('.real-num').html(answer)
+                        $('.startgame-btn').show()
+                        $('.submitans-btn').hide()
+                        $('.input').hide()
+                    }
                 }
             }
 
